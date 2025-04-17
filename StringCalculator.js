d91f6d3ddd2335484a4dfe780b4a9922d2d41a48
Vibha27 <svibha081@gmail.com>
2025-04-17 23:05:04 +0530

    handled function when string number is passed as an argument. added test case for the same as "return number when only number is provided"

--- StringCalculator.js
+++ StringCalculator.js
@@ -1,5 +1,8 @@
 function Add(numbers) {
   if (!numbers) return 0;
+  return parseInt(numbers)
 }
+
+console.log(Add("1"))
 
 module.exports = Add;
--- StringCalulator.test.js
+++ StringCalulator.test.js
@@ -3,3 +3,7 @@ const Add = require('./StringCalculator');
 test('returns 0 for empty string', () => {
   expect(Add("")).toBe(0);
 });
+
+test('return number when only number is provided', () => {
+    expect(Add("1")).toBe(1);
+})
